28189e6f9e20a6898e8c217b844a8af064974fcb
ThomasV <thomasv@electrum.org>
2019-03-12 19:54:17 +0100

    m

--- electrum/tests/test_forwarding.py
+++ electrum/tests/test_forwarding.py
@@ -6,7 +6,8 @@ class TestForwarding(unittest.TestCase):
 
     @staticmethod
     def run_shell(args, timeout=30):
-        process = subprocess.Popen(['electrum/tests/test_forwarding/test_forwarding.sh'] + args, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        process = subprocess.Popen(['electrum/tests/test_lnwatcher/' + cmd[0]] + ([] if len(cmd) == 1 else cmd[1:]), stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        #process = subprocess.Popen(['electrum/tests/test_forwarding/test_forwarding.sh'] + args, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
         for line in iter(process.stdout.readline, b''):
             sys.stdout.write(line.decode(sys.stdout.encoding))
         process.wait(timeout=timeout)
@@ -15,15 +16,16 @@ class TestForwarding(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.run_shell(['start'])
+        cls.run_shell(['setup.sh'])
+        cls.run_shell(['test_forwarding.sh', 'start'])
 
     @classmethod
     def tearDownClass(cls):
-        cls.run_shell(['stop'])
+        cls.run_shell(['test_forwarding.sh', 'stop'])
 
     def test_forwarding(self):
-        self.run_shell(['open'])
-        self.run_shell(['status'])
-        self.run_shell(['pay'])
-        self.run_shell(['status'])
-        self.run_shell(['close'])
+        self.run_shell(['test_forwarding.sh', 'open'])
+        self.run_shell(['test_forwarding.sh', 'status'])
+        self.run_shell(['test_forwarding.sh', 'pay'])
+        self.run_shell(['test_forwarding.sh', 'status'])
+        self.run_shell(['test_forwarding.sh', 'close'])
